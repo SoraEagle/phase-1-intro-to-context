df0b457c2a56659cabe563bd20e18922de157acb
Matthew <phillips.matthew829@gmail.com>
2021-12-03 15:33:13 -0600

    Done.

--- index.js
+++ index.js
@@ -1,20 +1,9 @@
 /*
-    Employees always check in and out on the hour
-    Timestamps provided as Strings "YYYY-MM-DD HHMM"
-    Employees will never work across days
-
 Bonus Challenges:
     Raise an exception if a timeIn is without a matching timeOut
     Figure out how to turn a timestamp into a construct that allows handling cross-day times and
         end times that aren't on the hour
     Raising errors if timestamp is in invalid format*/
-/*
-Function Signatures:
-    calculatePayroll:
-        Returns:
-            Sum of pay owed to all employees for all dates, as number
-        Behavior:
-            Using wagesEarnedOnDate, accumulate the value of all dates worked by the employee in the record used as context. Amount should be returned as number*/
 /*
 Current Time Complexity:
     createEmployeeRecord:
@@ -30,10 +19,9 @@ Current Time Complexity:
     wagesEarnedOnDate:
         O()
     allWagesFor:
-        O()
+        O(n)
     calculatePayroll:
-        O(?)
-*/
+        O(n) */
 function createEmployeeRecord(array){ //Object that will be used as an argument/parameter for other functions.
     let employee = {
         firstName: array[0],
@@ -47,11 +35,11 @@ function createEmployeeRecord(array){ //Object that will be used as an argument/
     return employee;
 }
 
-function createEmployeeRecords(arrays){ //Array of Arrays.
+function createEmployeeRecords(arrays){
     return arrays.map(element => createEmployeeRecord(element));
 }
 
-function createTimeInEvent(employee, time){ //
+function createTimeInEvent(employee, time){
     let timeInStamp = {
         type: "TimeIn",
         hour: parseInt(time.substr(11, 4)), //Ensure the hour variable is a number.
@@ -62,13 +50,13 @@ function createTimeInEvent(employee, time){ //
     return employee;
 }
 
-function createTimeOutEvent(employee, time){
+function createTimeOutEvent(employee, time){ //Create event for employee clocking out.
     let timeOutStamp = {
         type: "TimeOut",
         hour: parseInt(time.substr(11, 4)), //Ensure hour variable is a number.
         date: time.substr(0, 10)
     };
-    employee.timeOutEvents.push(timeOutStamp);
+    employee.timeOutEvents.push(timeOutStamp); //push timeOutStamp to employee's "punchcard".
     return employee;
 }
 
@@ -89,9 +77,9 @@ function wagesEarnedOnDate(employee, time){
 }
 
 function allWagesFor(employee){
-    let eligibleDates = employee.timeInEvents.map(element => element.date); //.map the test dates
+    let eligibleDates = employee.timeInEvents.map(element => element.date); //.map the test dates in array.
     let total = 0;
-    let wagesOwed = eligibleDates.reduce((previousValue, currentValue) => {
+    let wagesOwed = eligibleDates.reduce((previousValue, currentValue) => { //Calculate employee's wages.
         return previousValue + wagesEarnedOnDate(employee, currentValue);
     }, 0);
 
@@ -99,25 +87,10 @@ function allWagesFor(employee){
 }
 
 function calculatePayroll(array){
-    //Array OF employee records
-    let employees = array.map(employee => ({
-        firstName: employee.firstName, 
-        familyName: employee.familyName,
-        title: employee.title,
-        payPerHour: employee.payPerHour
-    }));
-    console.log(employees);
-
-    let employeeWages = allWagesFor(employees);
+    let employeeWages = array.map(employee => allWagesFor(employee));
     
-    let payroll = employees.reduce((previous, current) => {
-        //Calculate employee's total wages using allWagesFor(employee)
-        //use .reduce to find total allWagesFor for all employees(payroll)
-        //return 
+    let payroll = employeeWages.reduce((previous, current) => { //Calculate total wages owed to employees.
+        return previous + current;
     }, 0);
-    //return payroll
-
-    //Using wagesEarnedOnDate, accumulate value of all dates worked by employee in record used as context
-    //return sum of pay owed to ALL employees on all dates, as single sum
-    //use .reduce to create this sum
+    return payroll;
 }
